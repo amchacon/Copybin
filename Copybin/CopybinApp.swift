2170f36c9e5c716fdf38c3127689986228efb1ab
Alexandre Chacon <alexandrechacon@outlook.com>
2025-09-22 07:48:57 -0300

    Initial Commit

---
+++ Copybin/CopybinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopybinApp.swift
+//  Copybin
+//
+//  Created by Alexandre Chacon on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CopybinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CopybinTests/CopybinTests.swift
@@ -0,0 +1,17 @@
+//
+//  CopybinTests.swift
+//  CopybinTests
+//
+//  Created by Alexandre Chacon on 22/09/25.
+//
+
+import Testing
+@testable import Copybin
+
+struct CopybinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
